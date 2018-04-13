5bef51a21a4634abdee52edb302eed8e7a99fd82
fte <fte@spreadshirt.net>
2018-04-13 14:13:39 +0200

    added folder

---
+++ xdtools/style/style.py
@@ -0,0 +1,11 @@
+"""
+Contains the definition of Style.
+"""
+
+
+class Style(object):
+    """Represents a style of an artwork"""
+
+    def __init__(self, type_):
+        """Instantiate this Style"""
+        self.type = type_
